f84170ce1e54f1e49db404b3e01511782684f5dc
Максим Бацманов <markgusev995@gmail.com>
2025-09-12 18:01:32 +0300

    fixing before work

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(number_of_card: int) -> str:
+    """Функция, принимает номер карты и возвращает ее маску"""
+    digits_list = [int(d) for d in str(number_of_card)]
+
+    block_1 = "".join(map(str, digits_list[0:4]))
+    block_2 = "".join(map(str, digits_list[4:6]))
+    block_3 = "".join(map(str, digits_list[12:]))
+    return f"{block_1} {block_2}** **** {block_3}"
+
+
+def get_mask_account(number_of_account: int) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+    digits_list = [int(d) for d in str(number_of_account)]
+    last_block = "".join(map(str, digits_list[0:4]))
+    return f"**{last_block}"
+
