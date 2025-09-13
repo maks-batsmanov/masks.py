c04661cf20dc1df92b14809c11b5486410f8e022
Максим Бацманов <markgusev995@gmail.com>
2025-09-13 12:56:06 +0300

    added data types for functions

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,4 @@
-def get_mask_card_number(number_of_card: int) -> str:
+def get_mask_card_number(number_of_card: [int | str]) -> str:
     """Функция, принимает номер карты и возвращает ее маску"""
     digits_list = [int(d) for d in str(number_of_card)]
 
@@ -8,9 +8,8 @@ def get_mask_card_number(number_of_card: int) -> str:
     return f"{block_1} {block_2}** **** {block_3}"
 
 
-def get_mask_account(number_of_account: int) -> str:
+def get_mask_account(number_of_account: [int | str]) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
     digits_list = [int(d) for d in str(number_of_account)]
-    last_block = "".join(map(str, digits_list[0:4]))
+    last_block = "".join(map(str, digits_list[-4:]))
     return f"**{last_block}"
-
