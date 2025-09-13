1e0dbbf1293ebc09c1417c7ad388e1a8cc449d6a
Максим Бацманов <markgusev995@gmail.com>
2025-09-13 13:03:21 +0300

    added a date formatting feature

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,25 @@
-import masks
+import re
+
+from src import masks
+
 
 def mask_account_card(card_or_account: str) -> str:
-    '''Функция обрабатывает информацию о картах и счетах'''
-    if 'счет' in card_or_account.lower():
+    """Функция обрабатывает информацию о картах и счетах"""
+    if "счет" in card_or_account.lower():
         parts = card_or_account.split()
-        return f'{parts[0]} {masks.get_mask_account(parts[1])}'
+        number = parts[1]
+        return f"{parts[0]} {masks.get_mask_account(number)}"
     else:
         parts = card_or_account.split()
         if len(parts) == 2:
-            return f'{parts[0]} {masks.get_mask_card_number(parts[1])}'
+            number = parts[1]
+            return f"{parts[0]} {masks.get_mask_card_number(number)}"
         else:
-            return f'{parts[0]} {parts[1]} {masks.get_mask_card_number(parts[2])}'
+            number = parts[2]
+            return f"{parts[0]} {parts[1]} {masks.get_mask_card_number(number)}"
+
+
+def get_date(date: str) -> str:
+    """Функция принимает на вход дату и возвращает ее в формате 'ДД.ММ.ГГГГ'"""
+    dates = re.findall(r"\d{4}-\d{2}-\d{2}", date)
+    return f"{dates[0][8:]}.{dates[0][5:7]}.{dates[0][0:4]}"
