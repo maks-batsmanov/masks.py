7c75feace89430ef83fb00a43c066f6649a25b9b
Максим Бацманов <markgusev995@gmail.com>
2025-10-25 23:18:53 +0300

    update module exernal_api

--- src/external_api.py
+++ src/external_api.py
@@ -1,17 +1,16 @@
 import os
-from dotenv import load_dotenv
+
 import requests
+from dotenv import load_dotenv
 
 
-def get_amount_transactions(trans: dict): #-> float:
+def get_amount_transactions(trans: dict) -> float:
     """ Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях.
     Если транзакция была в USD или EUR, происходит обращение к внешнему API
     и конвертации суммы операции в рубли."""
 
-
     if trans["operationAmount"]["currency"]["code"] == "RUB":
-        return trans["operationAmount"]["amount"]
-
+        return float(trans["operationAmount"]["amount"])
     else:
         from_currency = trans["operationAmount"]["currency"]["code"]
         amount = trans["operationAmount"]["amount"]
@@ -19,30 +18,9 @@ def get_amount_transactions(trans: dict): #-> float:
         load_dotenv('.env')
         api_key = os.getenv('API_KEY')
         url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={from_currency}&amount={amount}"
-        payload = {}
         headers = {
             "apikey": api_key
         }
-
-        response = requests.request("GET", url, headers=headers, data = payload)
-        return response.text
-
-
-
-transactions = {
-    "id": 743628025,
-    "state": "EXECUTED",
-    "date": "2018-06-04T06:59:55.424356",
-    "operationAmount": {
-      "amount": "978.31",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Счет 54883981902864782073",
-    "to": "Счет 61834060137088759145"
-  }
-if __name__ == '__main__':
-    print(get_amount_transactions(transactions))
+        response = requests.request("GET", url, headers=headers)
+        data = response.json()
+        return float(data["result"])
