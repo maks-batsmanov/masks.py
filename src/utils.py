1f49833d191efe982293dbf0194e5dd41bab9053
Максим Бацманов <markgusev995@gmail.com>
2025-10-25 21:59:21 +0300

    edit for utils

--- src/utils.py
+++ src/utils.py
@@ -3,19 +3,18 @@ import os
 
 
 def get_transaction_data(path_to_file: str) -> list:
-    """Функция принимает путь к файлу, и возвращает список словарей с данными о транзакциях
+    """Функция принимает путь к json-файлу, и возвращает список словарей с данными о транзакциях
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список"""
 
     normalized_path = os.path.normpath(path_to_file)
 
-    if os.path.getsize(normalized_path) == 0:
-        return []
     if not os.path.exists(normalized_path):
         return []
-
+    if os.path.getsize(normalized_path) == 0:
+        return []
     with open(normalized_path, "r", encoding="utf-8") as file:
         content = json.load(file)
-    if not isinstance(normalized_path, list):
+    if not isinstance(content, list):
         return []
 
     return content
