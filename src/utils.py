22e0504691bd1586b78dc0ea9ddba7ea44deacc4
Максим Бацманов <markgusev995@gmail.com>
2025-10-29 22:43:16 +0300

    added logging in utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,19 @@
 import json
+import logging
 import os
+
+current_file_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_file_dir)  # Поднимаемся на уровень выше
+log_dir = os.path.join(project_root, 'logs')
+log_file = os.path.join(log_dir, 'masks.log')
+os.makedirs(log_dir, exist_ok=True)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(log_file, mode='w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(filename)s %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_transaction_data(path_to_file: str) -> list:
@@ -7,14 +21,17 @@ def get_transaction_data(path_to_file: str) -> list:
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список"""
 
     normalized_path = os.path.normpath(path_to_file)
-
+    logger.info(f'Начало работы функции. Входные данные: {path_to_file}')
     if not os.path.exists(normalized_path):
+        logger.warning('Ошибка, файл не найден')
         return []
     if os.path.getsize(normalized_path) == 0:
+        logger.warning('Ошибка, файл пустой')
         return []
     with open(normalized_path, "r", encoding="utf-8") as file:
         content = json.load(file)
     if not isinstance(content, list):
+        logger.warning('Ошибка, файл не является списком')
         return []
-
+    logger.info('Работа завершена успешно')
     return content
